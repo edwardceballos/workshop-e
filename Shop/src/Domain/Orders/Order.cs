10d09b7c186db473c17d919d4110e0d0557e1c4c
Edward Ceballos <edward.ceballos@gmail.com>
2023-09-13 13:47:51 -0500

    first commit

---
+++ Shop/src/Domain/Orders/Order.cs
@@ -0,0 +1,30 @@
+using Shop.Domain.Customers;
+using Shop.Domain.Products;
+
+namespace Shop.Domain.Orders;
+
+public class Order
+{
+
+
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public Order Create(Customer customer)
+    {
+        var order = new Order
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer.id
+        };
+        return order;
+    }
+    public readonly HashSet<LineItem> _lineItems = new();
+    public void Add(Product product)
+    {
+        var lineItem = new LineItem(Guid.NewGuid(),id, product.Id, product.Price);
+
+        _lineItems.Add(lineItem);
+
+    }
+
+}
---
+++ Shop/src/Domain/Products/Sku.cs
@@ -0,0 +1,27 @@
+namespace Shop.Domain.Products;
+public record Sku{
+
+
+private const int DefaultLength=15;
+
+public string Value{get; init;}
+private Sku(string value) => Value=value;
+public static Sku Create(string value){
+
+if(string.IsNullOrEmpty(value)){
+  return null;
+}
+if(value.Length !=  DefaultLength){
+
+    return null;
+
+}
+return new Sku(value);
+
+}
+
+
+
+
+
+}
